51fa92703611c0bdcf2cefa28ef1253ec6e78ab8
mazadegan <azadeganmalek@gmail.com>
2023-01-21 14:25:05 -0500

    added state management stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
+import { useState } from 'react'
 import Questionnaire from './Routes/Questionnaire'
 import Results from './Routes/Results'
 
 function App() {
+
+  // object that holds name, age, savings, debt, degree, boolean for if they are going to grad school, graduation date, and monthly savings percentage
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [savings, setSavings] = useState('')
+  const [debt, setDebt] = useState('')
+  const [degree, setDegree] = useState('')
+  const [gradSchool, setGradSchool] = useState(false)
+  const [gradDate, setGradDate] = useState('')
+  const [monthlySavings, setMonthlySavings] = useState('')
+
+  // object that holds all the data
+  const data = {
+    name: name,
+    age: age,
+    savings: savings,
+    debt: debt,
+    degree: degree,
+    gradSchool: gradSchool,
+    gradDate: gradDate,
+    monthlySavings: monthlySavings
+  }
+
+  // object that holds all the functions that update the data
+  const setData = {
+    setName: setName,
+    setAge: setAge,
+    setSavings: setSavings,
+    setDebt: setDebt,
+    setDegree: setDegree,
+    setGradSchool: setGradSchool,
+    setGradDate: setGradDate,
+    setMonthlySavings: setMonthlySavings
+  }
+
   return (
-    <div>
+    <div className='flex flex-col w-full h-screen justify-center items-center'>
       <Router>
         <Routes>
-          <Route path="/" element={<Questionnaire />} />
+          <Route path="/" element={<Questionnaire data={data} setData={setData} />} />
           <Route path="/results" element={<Results />} />
         </Routes>
       </Router>
--- src/Routes/Questionnaire.jsx
+++ src/Routes/Questionnaire.jsx
@@ -1,10 +1,22 @@
-function Questionnaire() {
+function Questionnaire(props) {
     return (
-        <div className="flex w-full h-screen justify-center items-center">
-            <div className="flex flex-col items-start w-1/2 border p-4 rounded-sm space-y-4">
+        <div className="flex w-full h-full justify-center items-center">
+            <div className="flex flex-col items-start w-1/2 border p-12 rounded space-y-4 shadow-sm">
                 <span className="text-2xl font-bold">Tell us about yourself.</span>
-                <div>
-                    <span>What is your name?</span>
+                <div className="flex flex-col w-full justify-start">
+                    <span className="flex">What is your name?</span>
+                    {/* input field that updates props.name using props.setName */}
+                    <input type="text" className="border rounded-sm p-2" value={props.name} onChange={(e) => props.setData.setName(e.target.value)} />
+                </div>
+                <div className="flex flex-col w-full justify-start">
+                    <span className="flex">How old are you?</span>
+                    {/* input field that updates props.age using props.setAge */}
+                    <input type="text" className="border rounded-sm p-2" value={props.age} onChange={(e) => props.setData.setAge(e.target.value)} />
+                </div>
+                {/* div that holds a prev and next button that are justified between */}
+                <div className="flex justify-between w-full">
+                    <button className="bg-yellow-400 text-white rounded-sm p-2 px-4 font-bold">Prev</button>
+                    <button className="bg-yellow-400 text-white rounded-sm p-2 px-4 font-bold">Next</button>
                 </div>
             </div>
         </div>
