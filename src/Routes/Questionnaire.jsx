9dfd33a78c4687fee2cb6ec2e950738c5249d32b
mazadegan <azadeganmalek@gmail.com>
2023-01-21 13:30:44 -0500

    a

--- src/Routes/Questionnaire.jsx
+++ src/Routes/Questionnaire.jsx
@@ -1,7 +1,12 @@
 function Questionnaire() {
     return (
-        <div>
-            <h1>Questionnaire</h1>
+        <div className="flex w-full h-screen justify-center items-center">
+            <div className="flex flex-col items-start w-1/2 border p-4 rounded-sm space-y-4">
+                <span className="text-2xl font-bold">Tell us about yourself.</span>
+                <div>
+                    <span>What is your name?</span>
+                </div>
+            </div>
         </div>
     );
 }
